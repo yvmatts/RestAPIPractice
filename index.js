bd7d5b7f3e8a8458d85606412d46379335fba9d5
Yashvardhan Mathur <work.yvm@gmail.com>
2019-01-30 22:15:33 +0530

    POST,PUT,DELETE done
    GET and GeoJson is left

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ const express = require('express');
 const app = express();
 const router = require('./routes/api.js');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+
+//Database Connection
+mongoose.connect("mongodb://localhost/taxigo",{useNewUrlParser: true});
+mongoose.connection.once('open',function(){
+  console.log("Connected");
+}).on('error',function(error){
+    console.log(error);
+});
+mongoose.Promise = global.Promise;
+
+
+//Middleware
+
+//BodyParser
 app.use(bodyParser.json());
+//API Routes
 app.use(router);
+//ErrorHandling
+app.use(function(err,req,res,next){
+  res.send({error:err.message});
+});
+
 
 app.listen(process.env.port || 3000,function(){
   console.log("Listening at Port 3000");
--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Taxi = require('../models/taxi.js');
+router.get('/taxi',function(req,res,next){
+  res.send({type:'GET'});
+});
 
-router.get('/taxi',function(req,res){
+router.post('/taxi',function(req,res,next){
+
+  var t = new Taxi(req.body);
+  t.save().then(function(taxi){
+    res.send(taxi);
+  }).catch(next);
 
 });
 
-router.post('/taxi',function(req,res){
-
+router.put('/taxi/:dl',function(req,res,next){
+  Taxi.findOneAndUpdate({dlNo:req.params.dl},req.body).then(function(){
+    Taxi.findOne({dlNo:req.params.dl}).then(function(taxi){
+      res.send(taxi);
+    });
+  }).catch(next);
 });
 
-router.put('/taxi/:id',function(req,res){
-
-});
-
-router.delete('/taxi/:id',function(req,res){
-
+router.delete('/taxi/:dl',function(req,res,next){
+  Taxi.findOneAndDelete({dlNo:req.params.dl}).then(function(taxi){
+    res.send(taxi);
+  }).catch(next);
 });
 
 module.exports = router;
